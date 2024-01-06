8af76a83d45c87b77f1475f3230f3be6e83b786f
Dawson Zhang <37853419+zfalstic@users.noreply.github.com>
2024-01-05 22:29:30 -0600

    Add files via upload

---
+++ src/game/Battle.java
@@ -0,0 +1,58 @@
+package game;
+
+public class Battle {
+	private Pokemon p1;
+	private Pokemon p2;
+	
+	public Battle(Pokemon p1, Pokemon p2) {
+		this.p1 = p1;
+		this.p2 = p2;
+	}
+	
+	public void playRound() {
+		this.getP1().attack(this.getP2());
+		if(this.getP2().getHealth() == 0) {
+			System.out.println("\n" + this.getP2().getName() + " has fainted. " + this.getP1().getName() + " has gained 40 exp.");
+			return;
+		}
+		System.out.println("\n" + this.getP2().getName() + " is now at " + this.getP2().getHealth() + " hp.");
+		
+		this.getP2().attack(this.getP1());
+		if(this.getP1().getHealth() == 0) {
+			System.out.println("\n" + this.getP1().getName() + " has fainted. " + this.getP2().getName() + " has gained 40 exp.");
+			return;
+		}
+		System.out.println("\n" + this.getP1().getName() + " is now at " + this.getP1().getHealth() + " hp.");
+	}
+	
+	public void playBattle() {
+		if(this.getP1().getHealth() == 0) {
+			return;
+		}
+		if(this.getP2().getHealth() == 0) {
+			return;
+		}
+		this.playRound();
+		this.playBattle();
+	}
+	
+	public Pokemon getP1() {
+		return this.p1;
+	}
+	
+	public void setP1(Pokemon p1) {
+		this.p1 = p1;
+	}
+	
+	public Pokemon getP2() {
+		return this.p2;
+	}
+	
+	public void setP2(Pokemon p2) {
+		this.p2 = p2;
+	}
+	
+	public String toString() {
+		return this.getP1().toString() + "\n" + this.getP2().toString();
+	}
+}
